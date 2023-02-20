6a821186ed3dded7b8b03a598419fe9d465de2e1
Frederic Marcotte <frederic.marcotte.2@ulaval.ca>
2023-02-19 19:13:59 -0500

    final

--- content/backend.py
+++ content/backend.py
@@ -97,9 +97,6 @@
 
 
 
-from IPython.display import clear_output
-
-
 # onde radio challenge
 
 INDICE_RADIO = "SUIVEZ LE CHEMIN DE L'ANCIENNE ÉPAVE, OÙ REPOSENT LES OSSEMENTS DES MARINS"
@@ -118,11 +115,9 @@ code = chaine[:5] + chaine[-2:]
 
 INDICE_DECOUPAGE = "NAGEZ DANS LE LABYRINTHE DE CORAIL, MAIS MÉFIEZ-VOUS DES CRÉATURES DANGEREUSES QUI S'Y CACHENT"
 
-def decode_decoupage():
-    clear_output(wait=True)
-    code_input = input("Entrez le code du bunker: ")
-    if code_input == code:
-        print("Code valide! Vous entrez dans le bunker et trouvez l'indice suivant:")
+def decode_decoupage(ans):
+    if ans == code:
+        print("Code valide! Vous trouvez l'indice suivant:")
         print(INDICE_DECOUPAGE)
     else:
         print("Code invalide! Veuillez exécuter la cellule (ctrl-enter) pour tenter à nouveau.")
@@ -131,11 +126,9 @@ def decode_decoupage():
 
 INDICE_STELE = 'TROUVEZ LA CARTE PERDUE DU ROI DES PIRATES, CACHÉE DANS UNE GROTTE AQUATIQUE'
 
-def decode_stele():
-    clear_output(wait=True)
-    code_input = input("Entrez le code secret de la stèle: ")
-    if code_input == "aY9bN":
-        print("Code valide! Une pierre de la stèle se déplace et vous trouvez l'indice suivant:")
+def decode_stele(ans):
+    if ans == "aY9bN":
+        print("Code valide! Vous trouvez l'indice suivant:")
         print(INDICE_STELE)
     else:
         print("Code invalide! Veuillez exécuter la cellule (ctrl-enter) pour tenter à nouveau.")
@@ -165,11 +158,9 @@ def pascal_triangle(n):
 
 INDICE_PASCAL = 'BRAVEZ LES ALGUES QUI SE TORDENT, ET VOUS TROUVEREZ LE COFFRE REMPLI DE RICHESSES'
 
-def decode_pascal():
-    clear_output(wait=True)
-    code_input = input("Entrez le code secret de la roche pyramidale: ")
-    if code_input == "120":
-        print("Code valide! La roche pyramidale se fragmente et vous trouvez l'indice suivant:")
+def decode_pascal(ans):
+    if ans == "120":
+        print("Code valide! Vous trouvez l'indice suivant:")
         print(INDICE_PASCAL)
     else:
         print("Code invalide! Veuillez exécuter la cellule (ctrl-enter) pour tenter à nouveau.")
